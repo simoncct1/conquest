679781e410a76132baabcb30de64a76b966888cc
simoncct1 <simoncouclet@gmail.com>
2023-03-16 22:44:58 +0100

    hehehehe

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-
+var today = new Date();
+var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+var time = today.getHours() + ":" + today.getMinutes();
+var dateTime = date+' '+time;
+document.getElementById('clock').innerHTML =
+    date +"</br>" + time;
+console.log(dateTime)
