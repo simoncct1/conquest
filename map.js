a3716a6300d3cdfb3f4ed553e894623e95017cb7
simoncct1 <simoncouclet@gmail.com>
2023-03-26 17:30:21 +0200

    control

--- map.js
+++ map.js
@@ -1,8 +1,12 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 // scene
 var scene = new THREE.Scene();
 // camera
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 1000);
+
+//control
+
 // renderer
 const sizes = {
      width: window.innerWidth/4,
@@ -13,4 +17,24 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
 document.body.appendChild(renderer.domElement);
-cu
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.update();
+//scene 
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+
+
+
+//anim
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
