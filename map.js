dc87e115ea51b16fbeb452a34ddae131a921ac86
simoncct1 <simoncouclet@gmail.com>
2023-03-26 17:19:16 +0200

    bigimprrr

---
+++ map.js
@@ -0,0 +1,16 @@
+import * as THREE from 'three';
+// scene
+var scene = new THREE.Scene();
+// camera
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 1000);
+// renderer
+const sizes = {
+     width: window.innerWidth/4,
+     height: window.innerHeight/4,
+}
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(sizes.width, sizes.height);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+document.body.appendChild(renderer.domElement);
+cu
