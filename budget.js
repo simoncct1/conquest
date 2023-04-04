59074c57feeb26d206b65f7bb71591314d228cb8
simoncct1 <simoncouclet@gmail.com>
2023-04-04 20:15:25 +0200

    responsive

--- budget.js
+++ budget.js
@@ -2,7 +2,7 @@
  var rows = document.getElementsByTagName("td");
 
  for (var i = 0; i < rows.length; i++) {
-    var valeur =  Math.random(10000, 999999);
+    var valeur =  Math.floor(Math.random() * (10000, 999999));
     rows[i].innerHTML = valeur;
     console.log(rows[i]);
     }
